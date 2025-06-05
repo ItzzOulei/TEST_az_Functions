5c23548bc2667f7b929f3689b98e9f9162accebc
Oulei <olivier.deszynski@ict-campus.net>
2025-06-05 13:35:16 +0200

    leck eier

--- src/functions/willy.js
+++ src/functions/willy.js
@@ -1,14 +1,22 @@
-const { app } = require('@azure/functions');
+const { app,input } = require('@azure/functions');
 
-app.http('leaderboard', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
-    }
+const cosmosInput = input.cosmosDB({
+    databaseName: 'DemoDatabase',
+    containerName: 'Items',
+    connection: 'CosmosDB',
+    sqlQuery: "select * from c"
 });
 
+app.http('getItems', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    extraInputs: [cosmosInput],
+    route: 'items',
+    handler: async (request, context) => {
+        const items = context.extraInputs.get(cosmosInput);
+        return {
+            body: JSON.stringify(items),
+            status: 200
+        };
+    }
+});
